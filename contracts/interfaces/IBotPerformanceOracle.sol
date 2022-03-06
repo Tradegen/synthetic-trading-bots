5a00f1b0ccef03d504107dc07c992ed95c3b62a6
Xavier Negron <xaviernegron888@gmail.com>
2022-03-05 22:54:23 -0600

    Create IBotPerformanceOracle.sol

---
+++ contracts/interfaces/IBotPerformanceOracle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+interface IBotPerformanceOracle {
+    // Views
+
+    /**
+     * @dev Returns the order info of the given order.
+     * @param _index Index of the order.
+     * @return (address, bool, uint256, uint256, uint256) Address of the asset, whether the order was a 'buy', timestamp, asset's price, new trading bot token price.
+     */
+    function getOrderInfo(uint256 _index) external view returns (address, bool, uint256, uint256, uint256);
+
+    /**
+     * @dev Returns the current token price of the trading bot.
+     * @return (uint256) Price of the trading bot's token, in USD.
+     */
+    function getTokenPrice() external view returns (uint256);
+
+    // Mutative
+
+    /**
+     * @dev Adds the order to the ledger and updates the trading bot's token price.
+     * @param _asset Address of the asset.
+     * @param _isBuy Whether the order is a 'buy' order
+     * @param _assetPrice Price of the asset when the order was executed.
+     */
+    function onOrderPlaced(address _asset, bool _isBuy, uint256 _assetPrice) external;
+}
