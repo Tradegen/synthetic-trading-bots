e5777a95af3d616f83620615243601300bc82dfa
Xavier Negron <xaviernegron888@gmail.com>
2022-03-10 17:24:08 -0600

    Use dedicated oracle instead of trading bot

--- contracts/BotPerformanceOracle.sol
+++ contracts/BotPerformanceOracle.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.3;
 
 // OpenZeppelin
 import "./openzeppelin-solidity/contracts/SafeMath.sol";
+import "./openzeppelin-solidity/contracts/Ownable.sol";
 
 // Interfaces
 import "./interfaces/IPriceAggregatorRouter.sol";
@@ -12,7 +13,7 @@ import "./interfaces/IPriceAggregator.sol";
 // Inheritance
 import "./interfaces/IBotPerformanceOracle.sol";
 
-contract BotPerformanceOracle is IBotPerformanceOracle {
+contract BotPerformanceOracle is IBotPerformanceOracle, Ownable {
     using SafeMath for uint256;
 
     struct Order {
@@ -26,19 +27,19 @@ contract BotPerformanceOracle is IBotPerformanceOracle {
     /* ========== STATE VARIABLES ========== */
 
     IPriceAggregatorRouter public immutable router;
-    address public immutable tradingBot;
+    address public oracle;
 
     uint256 numberOfOrders;
     mapping(uint256 => Order) public orders; // Starts at index 1.
 
     /* ========== CONSTRUCTOR ========== */
 
-    constructor(address _router, address _tradingBot) {
+    constructor(address _router, address _oracle) Ownable() {
         require(_router != address(0), "BotPerformanceOracle: invalid address for router.");
-        require(_tradingBot != address(0), "BotPerformanceOracle: invalid address for trading bot.");
+        require(_oracle != address(0), "BotPerformanceOracle: invalid address for oracle.");
 
         router = IPriceAggregatorRouter(_router);
-        tradingBot = _tradingBot;
+        oracle = _oracle;
     }
 
     /* ========== VIEWS ========== */
@@ -72,15 +73,15 @@ contract BotPerformanceOracle is IBotPerformanceOracle {
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
-   /**
+    /**
      * @dev Adds the order to the ledger and updates the trading bot's token price.
-     * @notice This function is meant to be called by the Trading Bot contract when the bot's keeper
+     * @notice This function is meant to be called by the dedicated oracle contract whenever the bot's keeper
      *          updates entry/exit rules with the latest asset price.
      * @param _asset Address of the asset.
      * @param _isBuy Whether the order is a 'buy' order
      * @param _price Price at which the order executed.
      */
-    function onOrderPlaced(address _asset, bool _isBuy, uint256 _price) external override onlyTradingBot {
+    function onOrderPlaced(address _asset, bool _isBuy, uint256 _price) external override onlyOracle {
         orders[numberOfOrders.add(1)] = Order({
             asset: _asset,
             isBuy: _isBuy,
@@ -93,10 +94,20 @@ contract BotPerformanceOracle is IBotPerformanceOracle {
         emit OrderPlaced(_asset, _isBuy, _price, orders[numberOfOrders].newBotTokenPrice);
     }
 
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function setOracle(address _newOracle) external {
+        require(_newOracle != address(0), "BotPerformanceOracle: invalid address for new oracle.");
+
+        oracle = _newOracle;
+
+        emit SetOracle(_newOracle);
+    }
+
     /* ========== MODIFIERS ========== */
 
-    modifier onlyTradingBot() {
-        require(msg.sender == tradingBot, "BotPerformanceOracle: only the TradingBot contract can call this function.");
+    modifier onlyOracle() {
+        require(msg.sender == oracle, "BotPerformanceOracle: only the dedicated oracle contract can call this function.");
         _;
     }
 
@@ -104,4 +115,5 @@ contract BotPerformanceOracle is IBotPerformanceOracle {
     /* ========== EVENTS ========== */
 
     event OrderPlaced(address asset, bool isBuy, uint256 assetPrice, uint256 newBotTokenPrice);
+    event SetOracle(address newOracle);
 }
