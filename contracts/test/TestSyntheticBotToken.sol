5cf04589dd154e4a592e7b7fdcccaa688781c1a6
Xavier Negron <xaviernegron888@gmail.com>
2022-03-11 10:11:07 -0600

    Create TestSyntheticBotToken.sol

---
+++ contracts/test/TestSyntheticBotToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+//Libraries
+import "../SyntheticBotToken.sol";
+
+contract TestSyntheticBotToken is SyntheticBotToken {
+    constructor(address _botPerformanceOracle, address _tradingBot, address _collateralToken, address _feePool)
+        SyntheticBotToken(_botPerformanceOracle, _tradingBot, _collateralToken, _feePool) {}
+
+    function setPosition(uint256 _positionIndex, uint256 _numberOfTokens, uint256 _createdOn, uint256 _rewardsEndOn, uint256 _lastUpdateTime, uint256 _rewardPerTokenStored, uint256 _rewardRate) external {
+        positions[_positionIndex] = Position({
+            numberOfTokens: _numberOfTokens,
+            createdOn: _createdOn,
+            rewardsEndOn: _rewardsEndOn,
+            lastUpdateTime: _lastUpdateTime,
+            rewardPerTokenStored: _rewardPerTokenStored,
+            rewardRate: _rewardRate
+        });
+    }
+}
